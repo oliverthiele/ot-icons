28b264c21d17ac1fb78a6fb77ab0dfbdc90efbc2
Oliver Thiele <mail@oliver-thiele.de>
2025-11-24 12:01:21 +0100

    [FEATURE] Adds PHP version requirement
    Introduces a PHP version requirement of 8.2 for compatibility.
    
    Adds the file `ext_emconf.php`

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF['ot_icons'] = [
+    'title' => 'Icon ViewHelper',
+    'description' => 'ViewHelper for rendering icons in your FluidTemplates. The output can be changed from one icon set to another at a later date.',
+    'category' => 'fe',
+    'author' => 'Oliver Thiele',
+    'author_email' => 'mail@oliver-thiele.de',
+    'author_company' => 'Web Development Oliver Thiele',
+    'state' => 'stable',
+    'version' => '1.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+            'php' => '8.2.0-8.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
